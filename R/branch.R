65c599af55ffe3d3e6f3e2a8d56c346a85d0701e
Urs Wilke <urs.wilke@gmail.com>
2021-10-13 00:21:27 +0200

    create benjamini branches with bezier instead of straight line

--- R/branch.R
+++ R/branch.R
@@ -16,32 +16,39 @@
 #'   ggforce::geom_bezier(ggplot2::aes(x = x, y = y, group = i)) +
 #'   ggplot2::coord_equal()
 benjamini_branch <- function(
-  df_branch = tibble::tibble(x1 = 0, x2 = 100, y1 = 40, y2 = 40),
+  df_bezier = tibble::tibble(
+    x = c(70, 84, 126, 168),
+    y = c(280, 245, 217, 217)
+  ),
   leaf_dist_approx = 20, leaf_angle = 45,
   first_dir = sample(0:1, 1),
   stalk_len = 15
 ) {
-  x1 <- df_branch$x1
-  x2 <- df_branch$x2
-  y1 <- df_branch$y1
-  y2 <- df_branch$y2
+  df_coords <- gen_bezier_coords(df_bezier)
 
-  dx <- x2 - x1
-  dy <- y2 - y1
+  n_points <- nrow(df_coords)
+  dx <- df_coords$x[-n_points] - df_coords$x[-1]
+  dy <- df_coords$y[-n_points] - df_coords$y[-1]
   angle <- atan2(dy, dx) / pi * 2 * 90
 
   branch_length <- sqrt((dx)^2 + (dy)^2)
 
-  n_leaves <- round(branch_length / leaf_dist_approx) - 1
+  n_leaves <- round(sum(branch_length) / leaf_dist_approx)
 
-  leaf_dist <- branch_length / (n_leaves + 1)
+  leaf_dist <- sum(branch_length) / (n_leaves + 1)
 
-  x_leaves <- seq(x1, x2, length.out = n_leaves + 1)[-1]
-  y_leaves <- seq(y1, y2, length.out = n_leaves + 1)[-1]
+  leaf_indices <- (1:n_leaves * leaf_dist) %>%
+    purrr::map_int(~which.max(.x < cumsum(branch_length)))
+  leaf_indices[length(leaf_indices)] <- n_points - 1
+
+  x_leaves <- df_coords$x[leaf_indices + 1]
+  y_leaves <- df_coords$y[leaf_indices + 1]
 
 
-  stalk_angle_pos <- angle + leaf_angle
-  stalk_angle_neg <- angle - leaf_angle
+  stalk_angle_pos <- angle - leaf_angle
+  stalk_angle_neg <- angle + leaf_angle
+  stalk_angle_pos <- stalk_angle_pos[leaf_indices]
+  stalk_angle_neg <- stalk_angle_neg[leaf_indices]
 
   dx_stalk_pos <- cos(stalk_angle_pos/90*pi/2) * stalk_len
   dy_stalk_pos <- sin(stalk_angle_pos/90*pi/2) * stalk_len
@@ -49,13 +56,13 @@ benjamini_branch <- function(
   dy_stalk_neg <- sin(stalk_angle_neg/90*pi/2) * stalk_len
 
   xends <- vector("numeric", n_leaves)
-  pos_positions <- (1:n_leaves + first_dir) %% 2 == 0
-  neg_positions <- (1:n_leaves + first_dir) %% 2 == 1
-  xends[pos_positions] <- x_leaves[pos_positions] + dx_stalk_pos
-  xends[neg_positions] <- x_leaves[neg_positions] + dx_stalk_neg
+  pos_positions <- (1:n_leaves + first_dir) %% 2 == 1
+  neg_positions <- (1:n_leaves + first_dir) %% 2 == 0
+  xends[pos_positions] <- x_leaves[pos_positions] - dx_stalk_pos[pos_positions]
+  xends[neg_positions] <- x_leaves[neg_positions] - dx_stalk_neg[neg_positions]
   yends <- vector("numeric", n_leaves)
-  yends[pos_positions] <- y_leaves[pos_positions] + dy_stalk_pos
-  yends[neg_positions] <- y_leaves[neg_positions] + dy_stalk_neg
+  yends[pos_positions] <- y_leaves[pos_positions] - dy_stalk_pos[pos_positions]
+  yends[neg_positions] <- y_leaves[neg_positions] - dy_stalk_neg[neg_positions]
 
   leaf_stalks <- tibble::tibble(
     x1 = x_leaves,
@@ -67,20 +74,20 @@ benjamini_branch <- function(
 
   l_leaf_bases <- leaf_stalks %>% dplyr::select(x1 = x2, y1 = y2) %>% purrr::transpose()
   leaf_angles <- vector("numeric", n_leaves)
-  leaf_angles[pos_positions] <- stalk_angle_pos
-  leaf_angles[neg_positions] <- stalk_angle_neg
+  leaf_angles[pos_positions] <- stalk_angle_pos[pos_positions]
+  leaf_angles[neg_positions] <- stalk_angle_neg[neg_positions]
 
   leaves <- purrr::map2_dfr(
     l_leaf_bases,
     leaf_angles,
-    ~benjamini_leaf(gen_benjamini_points(.x$x, .x$y), omega = .y),
+    ~benjamini_leaf(gen_benjamini_points(.x$x, .x$y), omega = .y + 180),
     .id = "leaf"
   ) %>%
     dplyr::mutate(type = "leaf_bezier") %>%
     tidyr::unite("i", .data$i, .data$leaf)
 
   dplyr::bind_rows(
-    df_branch,
+    df_bezier,
     leaf_stalks,
     leaves
   )
