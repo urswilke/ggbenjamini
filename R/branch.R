8995ec8cf6b120a619c01a8981d3b3b54d27750c
Urs Wilke <urs.wilke@gmail.com>
2021-10-13 16:51:29 +0200

    allow to manipulate the leaf sizes with a spark function

--- R/branch.R
+++ R/branch.R
@@ -1,12 +1,13 @@
 #' Generate a branch of benjamini leaves
 #'
 #' @param df_branch dataframe containing the x & y coordinates of a branch
-#' @param leaf_dist_approx approximate distance of two leaves
+#' @param leaf_mean_dist_approx approximate distance of two leaves
 #' @param leaf_angle angle between the leaf stalks and the branch
 #' @param first_dir direction of the first leaf in the branch (0 for right; 1 for left)
 #' @param stalk_len length of a stalk
+#' @param leave_size_dist Manipulate the sizes of the leaves with a spark function
 #'
-#' @return A dataframe conaining the data for leaves on a branch (see example).
+#' @return A dataframe containing the data for leaves on a branch (see example).
 #' @export
 #'
 #' @examples
@@ -20,9 +21,11 @@ benjamini_branch <- function(
     x = c(70, 84, 126, 168),
     y = c(280, 245, 217, 217)
   ),
-  leaf_dist_approx = 20, leaf_angle = 45,
+  leaf_mean_dist_approx = 10, leaf_angle = 45,
   first_dir = sample(0:1, 1),
-  stalk_len = 15
+  stalk_len = 15,
+  # Idea from flametree::flametree_grow()
+  leave_size_dist = spark_weibull(shape = 1.2, scale_factor = 0.5)
 ) {
   df_coords <- gen_bezier_coords(df_bezier)
 
@@ -31,15 +34,10 @@ benjamini_branch <- function(
   dy <- df_coords$y[-n_points] - df_coords$y[-1]
   angle <- atan2(dy, dx) / pi * 2 * 90
 
-  segment_lengths <- sqrt((dx)^2 + (dy)^2)
+  leaf_indices <- get_leaf_indices(dx, dy, leaf_mean_dist_approx, n_points)
 
-  n_leaves <- round(sum(segment_lengths) / leaf_dist_approx)
+  n_leaves <- length(leaf_indices)
 
-  leaf_dist <- sum(segment_lengths) / (n_leaves + 1)
-
-  leaf_indices <- (1:n_leaves * leaf_dist) %>%
-    purrr::map_int(~which.max(.x < cumsum(segment_lengths)))
-  leaf_indices[length(leaf_indices)] <- n_points - 1
 
   x_leaves <- df_coords$x[leaf_indices + 1]
   y_leaves <- df_coords$y[leaf_indices + 1]
@@ -77,7 +75,7 @@ benjamini_branch <- function(
   leaf_angles[pos_positions] <- stalk_angle_pos[pos_positions]
   leaf_angles[neg_positions] <- stalk_angle_neg[neg_positions]
 
-  dist_multiplicator <- dpois(1:n_leaves, 4)
+  dist_multiplicator <- leave_size_dist(n_leaves)
   dist_multiplicator <- dist_multiplicator/max(dist_multiplicator)
 
 
@@ -99,4 +97,41 @@ benjamini_branch <- function(
     leaves
   )
 
+}
+
+get_leaf_indices <- function(dx, dy, leaf_mean_dist_approx, n_points) {
+  segment_lengths <- sqrt((dx)^2 + (dy)^2)
+
+  branch_length <- sum(segment_lengths)
+  possible_leaf_dists <- branch_length /
+    leaf_mean_dist_approx *
+    exp(-(seq(0, 3, length.out = 100))) %>%
+    cumsum()
+  leaf_dists <- possible_leaf_dists[possible_leaf_dists <= branch_length]
+
+  leaf_indices <- leaf_dists %>%
+    purrr::map_int(~which.max(.x < cumsum(segment_lengths)))
+  if (max(leaf_indices < n_points - 1)) {
+    leaf_indices[length(leaf_indices) + 1] <- n_points - 1
+  }
+
+  leaf_indices
+}
+
+#' Manipulate the sizes of the leaves with a Weibull distribution
+#'
+#' This function returns a function which itself returns a numerical vector of
+#' length of the number of leaves on the branch.
+#'
+#' @param n_leaves The number of leaves on the branch
+#'
+#' @return dweibull() function with n_leaves as one of the arguments
+#' @export
+#'
+#' @examples
+#' spark_weibull()
+spark_weibull <- function(shape = 1.2, scale_factor = 0.5) {
+  function(n_leaves) {
+    dweibull(1:n_leaves, shape, scale = n_leaves * scale_factor)
+  }
 }
