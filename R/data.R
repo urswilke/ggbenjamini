e8fcd66b2e63476fa0ba94fac5b8b9c66b8b8287
Urs Wilke <urs.wilke@gmail.com>
2021-11-07 04:34:02 +0100

    document dataset

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Example bezier dataframe to grow leaves on
+#'
+#' Generated with `vignette("import_svg_bezier")`
+#'
+#' @format A data frame with 40 rows and 4 variables:
+#' \describe{
+#'   \item{i_branch}{branch index}
+#'   \item{svg_point_type}{original point type in the svg file }
+#'   \item{x}{x coordinate}
+#'   \item{y}{y coordinate}
+#'   ...
+#' }
+"df_bezier_skeleton"
